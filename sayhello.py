f211b0e7db7db5e9563ec93d3493285101217578
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2018-06-20 19:39:00 +0200

    added greeting constant

--- sayhello.py
+++ sayhello.py
@@ -1,9 +1,11 @@
 class SayHello:
+    GREETING_MESSAGE = "Hello {} !"
+
     def __init__(self, fullName):
         self.fullName = fullName
 
     def greet(self):
-        return 'Hello ' + self.fullName + ' !'
+        return self.GREETING_MESSAGE.format(self.fullName)
 
 s = SayHello('Philippe Brolly')
 print(s.greet())
